16bd2a3b498332d5e26b4ce65c32f681a985a30b
Hugo Locurcio <hugo.locurcio@hugo.pro>
2022-02-05 22:40:08 +0100

    Update scripts for Godot 3.4.1

--- build_windows.bat
+++ build_windows.bat
@@ -3,10 +3,10 @@
 :: Clean up old build directly in casse the previous build was aborted.
 rmdir /s /q %TEMP%\godot-debug-build
 
-git clone --branch="3.4-stable" --depth=1 https://github.com/godotengine/godot.git %TEMP%\godot-debug-build
+git clone --branch="3.4.1-stable" --depth=1 https://github.com/godotengine/godot.git %TEMP%\godot-debug-build
 pushd %TEMP%\godot-debug-build
-:: scons -j%NUMBER_OF_PROCESSORS% debug_symbols=yes progress=no
-:: scons -j%NUMBER_OF_PROCESSORS% debug_symbols=yes progress=no tools=no
+scons -j%NUMBER_OF_PROCESSORS% debug_symbols=yes progress=no
+scons -j%NUMBER_OF_PROCESSORS% debug_symbols=yes progress=no tools=no
 scons -j%NUMBER_OF_PROCESSORS% debug_symbols=yes progress=no tools=no target=release
 popd
 
@@ -16,4 +16,5 @@ popd
 mkdir bin
 mkdir bin\windows
 move %TEMP%\godot-debug-build\bin\*.exe bin\windows
+move %TEMP%\godot-debug-build\bin\*.pdb bin\windows
 rmdir /s /q %TEMP%\godot-debug-build
