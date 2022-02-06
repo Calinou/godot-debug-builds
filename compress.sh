93214275e6927198b00564bc26d7fbdc28561cc1
Hugo Locurcio <hugo.locurcio@hugo.pro>
2022-02-06 01:32:41 +0100

    Update scripts for Godot 3.4.2

--- compress.sh
+++ compress.sh
@@ -5,7 +5,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 # This script should be run after running both `build_linux.sh` and `build_windows.bat`.
 
-GODOT_VERSION="3.4.1"
+GODOT_VERSION="3.4.2"
 
 parallel 7z a -mx9 "{}.zip" "{}" ::: bin/linux/*.64
 
